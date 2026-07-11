cc2c9d21e82d6047d9f043ca0748c19c83417d8d
Nikushajgarkava45 <nikushajgarkava3@gmail.com>
2026-07-12 01:43:55 +0400

    fix viewer js / styles.css

--- viewer.js
+++ viewer.js
@@ -1,18 +1,29 @@
+import * as pdfjsLib from "./pdfjs/pdf.mjs";
+pdfjsLib.GlobalWorkerOptions.workerSrc = "./pdfjs/pdf.worker.mjs";
 const params = new URLSearchParams(window.location.search);
 const pdfURL = params.get("pdf");
-const viewer = document.getElementById("viewer");
-if (pdfURL) {
-  viewer.innerHTML = `
-        <h2>PDF Loaded</h2>
-
-        <p>${pdfURL}</p>
-
-        <hr>
-
-        <p>
-            pdf.js will be connected here.
-        </p>
-    `;
-} else {
-  viewer.innerHTML = "<h2>No PDF selected.</h2>";
+const canvas = document.getElementById("pdfCanvas");
+const context = canvas.getContext("2d");
+async function renderPDF() {
+  if (!pdfURL) {
+    alert("PDF not found.");
+    return;
+  }
+  try {
+    const pdf = await pdfjsLib.getDocument(pdfURL).promise;
+    const page = await pdf.getPage(1);
+    const viewport = page.getViewport({
+      scale: 1.5,
+    });
+    canvas.width = viewport.width;
+    canvas.height = viewport.height;
+    await page.render({
+      canvasContext: context,
+      viewport,
+    }).promise;
+    console.log("PDF Loaded");
+  } catch (error) {
+    console.error(error);
+  }
 }
+renderPDF();
